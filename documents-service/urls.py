407c65f6c5e9443f95a1cf12bcb80cc9084ca700
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-03-28 08:41:22 +0100

    Cleanup urls

--- documents-service/urls.py
+++ documents-service/urls.py
@@ -1,21 +1,5 @@
-"""documents URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path, re_path
-from documents.views import document_download_view, document_thumbnail_view
 from rest_framework import permissions
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
@@ -29,7 +13,7 @@ from drf_yasg import openapi
 swagger_info = openapi.Info(
         title="Documents Service API",
         default_version='latest',
-        description="Test description",
+        description="A microservice which handles file up- and downloads to an Amazon S3 Bucket.",
 )
 
 schema_view = get_schema_view(
@@ -44,10 +28,8 @@ urlpatterns = [
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
-    path('', include('api.urls')),
     path('health_check/', include('health_check.urls')),
-    re_path(r'^file/(?P<id>\w+)/$', document_download_view),
-    re_path(r'^thumbnail/(?P<id>\w+)/$', document_thumbnail_view),
+    path('', include('api.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns() \
